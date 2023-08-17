493797fe8002bf6d3419fed6735f00f022b8ae18
andrewpbray <andrewbray@berkeley.edu>
2023-08-17 00:23:49 -0700

    finish draft version

--- _extensions/stat20-notes/include-file-after.lua
+++ _extensions/stat20-notes/include-file-after.lua
@@ -1,15 +1,20 @@
 
-function add_include_after(pandoc)
+function add_include_after(doc)
   
-  for k,v in pairs(pandoc.meta) do
+  for k,v in pairs(doc.meta) do
     quarto.log.output(k)
   end
   
-  file_path = pandoc.meta["include-file-after"][1].text
-          
-  quarto.log.output(io.popen(file_path):lines())
-
-  return pandoc
+  filename = doc.meta["include-file-after"][1].text
+  file = io.open(filename, "r")
+  input = file:read("*a")
+  additional_content = pandoc.read(input, "markdown", PANDOC_READER_OPTIONS)
+  
+  for i = 1, #additional_content.blocks do
+    table.insert(doc.blocks, additional_content.blocks[i])
+  end
+  
+  return doc
 end
 
 return {
